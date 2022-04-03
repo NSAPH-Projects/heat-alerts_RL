68859788481768735b08b30dfaf05444a9b7ac68
Ellen Considine <ellen_considine@g.harvard.edu>
2022-04-03 14:17:34 -0400

    Getting land area from the US county shapefile

---
+++ code/Extract_csv_info.R
@@ -0,0 +1,14 @@
+## First, downloaded this shapefile from https://www2.census.gov/geo/tiger/TIGER2010/COUNTY/2010/
+
+setwd("C:/Users/ellen/OneDrive/MyDocs/Graduate Research/Counties_shapefile")
+
+library(raster)
+
+shp<- shapefile("tl_2010_us_county10.shp")
+
+
+df<- data.frame(fips = shp$GEOID10, Area = shp$ALAND10)
+
+df$Area<- as.numeric(df$Area)*(3.86102e-7) # converting square meters to square miles
+
+write.csv(df, "Counties_land_area.csv", row.names = FALSE)
