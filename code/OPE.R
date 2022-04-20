ec87a78cd98ef7fcf4df7269eea0cd771beaf89a
Ellen Considine <ellen_considine@g.harvard.edu>
2022-04-20 19:12:42 -0400

    continued running OPE

--- code/OPE.R
+++ code/OPE.R
@@ -1,4 +1,6 @@
 
+library(dplyr)
+
 setwd("/n/dominici_nsaph_l3/projects/heat-alerts_mortality_RL")
 
 load("data/HARL_prelim_image.RData")
@@ -53,28 +55,55 @@ OPE<- function(S_test, A_test, w, y, q_tol){
 }
 
 ## Set up test set:
-y<- 100
+N<- length(test)
+n<- 152 # number of days in each summer (episode)
+M<- N/n # number of episodes
+
 S_test<- S[test,]
 A_test<- Actions[test]
+discount_vec<- rep(cumprod(rep(discount, n))/discount, M)
 failed_alerts<- (A_test == 1) & (Not_Hot[test] == 1)
 Rewards<- (-1*(summer$N*100000/summer$Population))[-seq(153, nrow(summer), 153)]
 
 Pi_b<- pi_b(S_test, A_test)
 
-N<- length(test)
-n<- 152 # number of days in each summer (episode)
-M<- N/n # number of episodes
 
-discount_vec<- rep(cumprod(rep(discount, n))/discount, M)
+#### Test for y = 10: 
 
+OPE(S_test, A_test, w = c(),
+    y = 10, q_tol = 0.00175)
 
-#### Test: 
+OPE(S_test, A_test, w = c(),
+    y = 10, q_tol = 0.001)
+
+#### Test for y = 100: 
 
 OPE(S_test, A_test, w = c(4.9002988, 0.9561502, 122.8304463, -467.6929355, 
                           -626.8763427, 5.3314790, 0.5666687, 125.3147654, 
                           -467.0453364, -616.1291645),
     y = 100, q_tol = 0.0025) # -91.86163
 
+OPE(S_test, A_test, w = c(3.227026, -1.057144, 157.743203, -397.232099,
+                          -915.638060, 2.3019162, -0.1524963, 158.0077266,
+                          -396.6269728, -903.3962598),
+    y = 100, q_tol = 0.002) # -87.37957
 
+OPE(S_test, A_test, w = c(2.0424366, -2.5993480, 241.4489724, -630.5807619,
+                          -1430.6157089, 0.6076141, -0.6964970, 241.2610769,
+                          -629.8293962, -1415.5634076),
+    y = 100, q_tol = 0.00175) # -87.43654
 
+OPE(S_test, A_test, w = c(1.0884783, -4.2786934, 342.4826609, -827.0148787,
+                          -1696.9958081, -0.7456498, -1.4345054, 342.1957501,
+                          -826.1844939, -1679.2944142),
+    y = 100, q_tol = 0.0015) # -86.28342
+
+OPE(S_test, A_test, w = c(0.5592941, -5.3593660, 477.8782490, -1710.4075695,
+                          -635.3351123, -1.0490838, -1.7841658, 477.4670577,
+                          -1708.6196682,  -615.0188748),
+    y = 100, q_tol = 0.001) # -89.0691
+
+OPE(S_test, A_test, w = c(4.357196, -5.755611, 603.492995, -2671.301377, 251.558483,
+                          4.136001, -1.775227, 602.964898, -2667.975966, 274.604229),
+    y = 100, q_tol = 0.0005) # -90.25862
 
