96848b3a91ef8e38c92cb7a44a9dd08e66ea415c
Ellen Considine <ellen_considine@g.harvard.edu>
2022-08-16 14:50:31 -0400

    obtained optimal policy sequentially using the final Q model

--- code/Q-learning.R
+++ code/Q-learning.R
@@ -147,18 +147,21 @@ batch_size<- 300000
 
 iter<- 1
 
+lm_Q<- readRDS("Aug_results/Lm_8-2.rds")
+
 s<- Sys.time()
 Q_model<- glm(Target ~ A*., 
-              data = data.frame(Target, S, A, Pop.65=Train$Pop.65[-seq(n_days, nrow(Train), n_days)]),
-              family = poisson, offset = Pop.65,
+              data = data.frame(Target, S, A),
+              family = poisson, offset = log(Train$Pop.65[-seq(n_days, nrow(Train), n_days)]),
               subset = sample(1:length(Target), batch_size, replace = FALSE),
-              control = list(maxit = 1), warning = FALSE)
+              # control = list(maxit = 1), 
+              start = lm_Q$coefficients)
 # e<- Sys.time()
 # e-s
 # Q_model<- lm(Target ~ A*., data = data.frame(Target, S[positive,], A))
 old_coefs<- rnorm(length(coef(Q_model)))
 
-sink("Aug_results/First_SGD.txt")
+sink("Aug_results/First_poisson_SGD.txt")
 
 while(sum((coef(Q_model)-old_coefs)^2, na.rm = TRUE) > 0.1 & iter < 1000){
   
@@ -192,10 +195,10 @@ e-s
 
 sink()
 
-saveRDS(Q_model, "Aug_results/Lm_8-16.rds")
+saveRDS(Q_model, "Aug_results/Glm_8-16.rds")
 saveRDS(Target, "Aug_results/Targets_8-16.rds")
 
-Q_model<- readRDS("Aug_results/Lm_8-16.rds")
+Q_model<- readRDS("Aug_results/Glm_8-16.rds")
 Target<- readRDS("Aug_results/Targets_8-16.rds")
 
 ### Then a couple of times with full data...
@@ -225,19 +228,76 @@ coef(Q_model)
 # Budget<- rep(last_day$alert_sum, each = (n_days - 1))
 # ID<- rep(1:(n_counties*n_years), each = (n_days-1))
 
+#####################################################################
+
+Q_model<- readRDS("Aug_results/Lm_8-2.rds")
+Target<- readRDS("Aug_results/Targets_8-2.rds")
+
+Q_model<- glm(Target ~ A*.,  
+              data = data.frame(Target, S, A),
+              family = gaussian,
+              # control = list(maxit = 1), 
+              start = coef(Q_model))
+
 ## Get budget and which ones are over for the training set:
 budget<- Train[which(Train$dos == 153), "alert_sum"]
 Budget<- rep(budget, each = (n_days - 1))
 
-Q_mat<- eval_Q(S.1, Q_model)
-max.Q<- rowMaxs(Q_mat)
-argmax<- max.col(Q_mat, ties.method = "first") - 1
-cumsum_q<- as.vector(t(aggregate(argmax ~ ID,
-                                 data.frame(ID, argmax), cumsum)[[2]]))
-over_budget<- which(cumsum_q > Budget)
-argmax[over_budget]<- 0
-max.Q[over_budget]<- Q_mat[over_budget, 1]
-policy<- argmax
+m<- mean(States$alert_sum)
+s<- sd(States$alert_sum)
+
+## Hack way to evaluate because the predict function is being dumb:
+df0<- model.matrix(Q_model)
+df0[,"A"]<- 0
+df0[,20:36]<- 0
+
+df1<- model.matrix(Q_model)
+df1[,"A"]<- 1
+df1[,20:36]<- df1[,3:19]
+
+new_alerts<- rep(0, nrow(S))
+policy<- rep(0, nrow(S))
+
+for(i in 1:max(ID)){
+  pos<- which(ID == i)
+  
+  d<- 1
+  while(new_alerts[pos[d]] < Budget[pos[d]]){
+  # while(d < n_days){
+    new_scaled<- (new_alerts[pos[d]] - m)/s
+    v0<- df0[pos[d],]
+    v0["alert_sum"]<- new_scaled
+    q0<- coef(Q_model) %*% v0
+    v1<- df1[pos[d],]
+    v1["alert_sum"]<- new_scaled
+    v1["A:alert_sum"]<- new_scaled
+    q1<- coef(Q_model) %*% v1
+    if(q1 > q0){
+      policy[pos[d]]<- 1
+      new_alerts[pos[d:(n_days-1)]]<- new_alerts[pos[d:(n_days-1)]] + 1
+    }
+    d<- d+1
+  }
+}
+
+saveRDS(policy, "Aug_results/Policy_8-2.rds")
+
+# df0[,"alert_sum"]<- (new_alerts - m)/s
+# Q0<- t(coef(Q_model) %*% t(df0))
+# df1[,"alert_sum"]<- (new_alerts - m)/s
+# df1[,"A:alert_sum"]<- df1[,"alert_sum"]
+# Q1<- t(coef(Q_model) %*% t(df1))
+# Q_mat<- cbind(Q0, Q1)
+# 
+# max.Q<- rowMaxs(Q_mat)
+# argmax<- max.col(Q_mat, ties.method = "first") - 1
+# cumsum_q<- as.vector(t(aggregate(argmax ~ ID,
+#                                  data.frame(ID, argmax), cumsum)[[2]]))
+# over_budget<- which(cumsum_q > Budget)
+# argmax[over_budget]<- 0
+# max.Q[over_budget]<- Q_mat[over_budget, 1]
+# policy<- argmax
+
 
 #### Inspect:
 
