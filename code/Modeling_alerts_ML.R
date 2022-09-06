9af30db545b8cfa9c1a97a18ea03389cd768c7e5
Ellen Considine <ellen_considine@g.harvard.edu>
2022-09-06 09:32:23 -0400

    modeling alerts with RF

---
+++ code/Modeling_alerts_ML.R
@@ -0,0 +1,105 @@
+library(ggplot2)
+library(dplyr)
+library(caret)
+library(parallel)
+library(doParallel)
+
+load("data/Train-Valid-Test.RData")
+
+n_counties<- length(unique(Train$GEOID))
+n_years<- 11
+n_days<- 153
+
+n_cv<- 5
+
+set.seed(321)
+eda_set<- Train[sample(1:nrow(Train), 0.5*nrow(Train), replace = FALSE),]
+Eda_set<- data.frame(scale(eda_set[,vars<- c("HImaxF_PopW", "quant_HI_county", 
+                                             "quant_HI_yest_county",
+                                             "quant_HI_3d_county", 
+                                             "quant_HI_fwd_avg_county",
+                                             "Pop_density", "Med.HH.Income",
+                                             "year", "dos",
+                                             "alert_sum")]), 
+                     alert = factor(eda_set$alert),
+                     dow = factor(eda_set$dow), data.frame(scale(eda_set[,vars<- c("HImaxF_PopW", "quant_HI_county", 
+                                             "quant_HI_yest_county",
+                                             "quant_HI_3d_county", 
+                                             "quant_HI_fwd_avg_county",
+                                             "Pop_density", "Med.HH.Income",
+                                             "year", "dos",
+                                             "alert_sum")]), 
+                     alert = factor(eda_set$alert),
+                     dow = factor(eda_set$dow), 
+                     holiday = factor(eda_set$holiday),
+                     Zone = factor(eda_set$BA_zone))
+levels(Eda_set$alert)<- c("none", "alert")
+                     holiday = factor(eda_set$holiday),
+                     Zone = factor(eda_set$BA_zone))
+levels(Eda_set$alert)<- c("none", "alert") # so caret can predict probabilities
+
+IND<- createFolds(Eda_set$alert, n_cv, returnTrain = TRUE)
+
+myControl<- trainControl(#method = "cv", number = n_cv, 
+                          classProbs = TRUE,
+                          search = "grid", index = IND,
+                          savePredictions = "final",
+                          verboseIter = TRUE, allowParallel = FALSE) #TRUE
+
+tgrid<- expand.grid( .mtry = 15, .splitrule = "extratrees", .min.node.size = 1)
+
+## Set up parallelization:
+
+# cluster<- makeCluster(10)
+# registerDoParallel(cluster)
+
+## Run model:
+
+s<- Sys.time()
+
+ranger_model <- train(alert ~ ., data = Eda_set, method = "ranger",
+                      trControl = myControl, tuneGrid = tgrid)
+
+e<- Sys.time()
+e-s
+# stopCluster(cluster)
+
+saveRDS(ranger_model, "Aug_results/a_RF_9-5_50pct.rds")
+
+
+#### Assess predictions:
+
+ranger_model<- readRDS("Aug_results/a_RF_9-5_50pct.rds")
+
+## Training:
+
+tab<- table(ranger_model$pred$pred, ranger_model$pred$obs)
+sensitivity(tab)
+specificity(tab)
+posPredValue(tab)
+negPredValue(tab)
+
+## Validation: (on the rest of the training data)
+
+valid<- Train[which(! as.numeric(row.names(Train)) %in% as.numeric(row.names(eda_set))),]
+Valid<- data.frame(scale(valid[,vars<- c("HImaxF_PopW", "quant_HI_county", 
+                                           "quant_HI_yest_county",
+                                           "quant_HI_3d_county", 
+                                           "quant_HI_fwd_avg_county",
+                                           "Pop_density", "Med.HH.Income",
+                                           "year", "dos",
+                                           "alert_sum")]), 
+                   alert = factor(valid$alert),
+                   dow = factor(valid$dow), 
+                   holiday = factor(valid$holiday),
+                   Zone = factor(valid$BA_zone))
+levels(Valid$alert)<- c("none", "alert")
+
+preds<- predict(ranger_model, Valid) # to get probabilities (for OPE), use type = "prob"
+
+
+
+
+
+
+
