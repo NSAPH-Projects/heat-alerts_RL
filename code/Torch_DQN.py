cb61e1d8b7bebcd6624ada58a248d9772a76232a
Ellen Considine <ellen_considine@g.harvard.edu>
2022-10-05 18:19:03 -0400

    turned Q_prep into a function

--- code/Torch_DQN.py
+++ code/Torch_DQN.py
@@ -1,5 +1,23 @@
 
-# ! python /n/dominici_nsaph_l3/Lab/projects/heat-alerts_mortality_RL/code/Q_prep.py
+# import os
+import time
+import numpy as np
+import pandas as pd
+# import itertools
+# import matplotlib.pyplot as plt
+from scipy.special import expit, softmax
+
+#%%
+
+import torch 
+from torch import nn # creating modules
+from torch.nn import functional as F # losses, etc.
+from torch.utils.data import TensorDataset, DataLoader
+from torch import optim
+from tqdm import tqdm
+from copy import deepcopy
+
+from code.Q_prep_function import make_data
 
 ## Set up the model
 
@@ -7,11 +25,11 @@ class DQN(nn.Module):
     def __init__(self, n_col) -> None:
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_col, n_col**2),
+            nn.Linear(n_col, n_col^2),
             nn.SiLU(),
             # nn.Linear(n_col**2, n_col**2),
             # nn.SiLU(),
-            nn.Linear(n_col**2, 2)
+            nn.Linear(n_col^2, 2)
         )
     def forward(self, x):
         return self.net(x)
@@ -28,8 +46,17 @@ def eval_Q_double(Q_model, Qtgt_model, S, over = None):
         return final_Q
     return best_Q
 
+
 ## Set up dataloader
+D = make_data()
+S,A,R,S_1,ep_end,over = [D[k] for k in ("S","A","R","S_1","ep_end","over")]
 data = [S.drop("index", axis = 1), A, R, S_1.drop("index", axis = 1), ep_end, over]
+
+gamma = 0.99
+
+n_years = 11
+n_days = 153
+n_counties = S.shape[0]/(n_years*(n_days-1))
 
 dev = "cpu"
 # dev = "cuda"
@@ -62,12 +89,14 @@ tgt_model = deepcopy(model)
 
 ## Train
 
-epochs = 10000
+epochs = 20000
 
 epoch_loss_means = []
 epoch_loss_full = []
 start = time.time()
-for k in range(1, epochs):
+for k in range(13975, epochs):
+    # if k == 100:
+    #     break
     l = []
     iter = 1
     for s, a, r, s1, e, o in tqdm(DL, disable=True):
@@ -105,35 +134,37 @@ for k in range(1, epochs):
 print("--- %s seconds ---" % (time.time() - start))
 
 
-torch.save(model, "Fall_results/DQN_9-23.pt")
+torch.save(model, "Fall_results/DQN_9-27.pt")
 ## Convert these to pd dataframes and then .to_csv
 EL = pd.DataFrame(epoch_loss_means, columns = ["Means"])
 EL["Full"] = epoch_loss_full
-EL.to_csv("Fall_results/DQN_9-23_epoch-losses.csv")
+EL.to_csv("Fall_results/DQN_9-27_epoch-losses.csv")
 
 
-### Look at results from model:
-model = torch.load("Fall_results/DQN_9-23.pt")
-S = S.drop("index", axis = 1)
+# ### Look at results from model:
+# model = torch.load("Fall_results/DQN_9-23.pt")
+# S = S.drop("index", axis = 1)
 
-Constraint = pd.DataFrame(Budget).drop(n_seq_s)
-new_alerts = np.zeros(len(ID))
-policy = np.zeros(len(ID))
-for i in range(0, max(ID)):
-    pos = np.where(np.array(ID) == i)
-    d = 0
-    while (d < 152) & (new_alerts[pos[0][d]] < Constraint.iloc[pos[0][d]]):
-        alerts_scaled = (new_alerts[pos[0][d]] - s_means["alert_sum"])/s_stds["alert_sum"]
-        more_scaled = (Constraint.iloc[pos[0][d]] - new_alerts[pos[0][d]] - s_means["More_alerts"])/s_stds["More_alerts"]
-        new_s = S.iloc[pos[0][d]]
-        new_s["alert_sum"] = alerts_scaled
-        new_s["More_alerts"] = more_scaled
-        v = torch.tensor(new_s)
-        output = model(v.float()).detach().numpy()
-        if output[1] > output[0]:
-            policy[pos[0][d]] = 1
-            new_alerts[pos[0][d:(n_days-1)]] += 1
-        d+=1
+# Constraint = pd.DataFrame(Budget).drop(n_seq_s)
+# new_alerts = np.zeros(len(ID))
+# policy = np.zeros(len(ID))
+# for i in range(0, max(ID)):
+#     pos = np.where(np.array(ID) == i)
+#     d = 0
+#     while (d < 152) & (new_alerts[pos[0][d]] < Constraint.iloc[pos[0][d]]):
+#         alerts_scaled = (new_alerts[pos[0][d]] - s_means["alert_sum"])/s_stds["alert_sum"]
+#         more_scaled = (Constraint.iloc[pos[0][d]] - new_alerts[pos[0][d]] - s_means["More_alerts"])/s_stds["More_alerts"]
+#         new_s = S.iloc[pos[0][d]]
+#         new_s["alert_sum"] = alerts_scaled
+#         new_s["More_alerts"] = more_scaled
+#         v = torch.tensor(new_s)
+#         output = model(v.float()).detach().numpy()
+#         if output[1] > output[0]:
+#             policy[pos[0][d]] = 1
+#             new_alerts[pos[0][d:(n_days-1)]] += 1
+#         d+=1
+
+#############
 
 # Q = model(tensors[0].float()) # n x 2
 # best_action = Q.argmax(axis=1).view(-1, 1)
