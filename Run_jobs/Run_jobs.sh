823bf23383383ac1ac54eb4104f81c8704a1ae87
Ellen Considine <ellen_considine@g.harvard.edu>
2022-11-04 10:19:07 -0400

    Update Run_jobs.sh

--- Run_jobs/Run_jobs.sh
+++ Run_jobs/Run_jobs.sh
@@ -2,14 +2,14 @@
 #SBATCH -N 1
 #SBATCH -n 4
 #SBATCH -p fasse_gpu
-#SBATCH -t 0-12:00
+#SBATCH -t 1-00:00
 #SBATCH --mem 32G
 #SBATCH --gres gpu:1
 #SBATCH --array 0-1
 #SBATCH -o ./slurm/dqn.%a.out
+
 #
 source ~/.bashrc
-
-
-
+mymodules
 source activate pt1.12_cuda11.6
+python scripts/pylauncher.py --job_file="./scripts/DQN_tests" --i $SLURM_ARRAY_TASK_ID
