c7a291023ec2c631ec8f97b1b19ba4e287022b4a
Ellen Considine <ellen_considine@g.harvard.edu>
2022-11-14 18:23:40 -0500

    allowing run from command line

--- heat_alerts/VisRat_OPE.R
+++ heat_alerts/VisRat_OPE.R
@@ -5,9 +5,25 @@ library(caret)
 
 load("data/Small_S-A-R_prepped.RData")
 
+args<- commandArgs(trailing = TRUE) # outcome (string), date (string), constrained (bool)
+
+outcome<- args[1]
+date<- args[2]
+constrained<- args[3]
+
 ## Visitation ratios:
 
-ranger_model<- readRDS("Fall_results/VisRat_11-8_hosps_constrained_75pct.rds")
+if(constrained == TRUE){
+  ranger_model<- readRDS(paste0("Fall_results/VisRat_", date, "_", # 11-8
+                                outcome, "_constrained_75pct.rds"))
+  new_pol<- read.csv(paste0("Fall_results/DQN_", date, "_", # 11-5
+                            outcome, "_constrained_policy.csv"))$policy
+}else{
+  ranger_model<- readRDS(paste0("Fall_results/VisRat_", date, "_", 
+                                outcome, "_75pct.rds"))
+  new_pol<- read.csv(paste0("Fall_results/DQN_", date, "_", 
+                            outcome, "_policy.csv"))$policy
+}
 
 VR_probs<- predict(ranger_model, DF, type = "prob")
 VR_preds<- VR_probs$New/VR_probs$Behavior
@@ -21,7 +37,6 @@ behav<- behav[-seq(n_days, length(behav), n_days)]
 pb<- behav
 pb[which(A == 0)]<- 1-behav[which(A == 0)]
 
-new_pol<- read.csv("Fall_results/DQN_11-5_hosps_constrained_policy.csv")$policy
 
 pg<- as.numeric(new_pol == A)
 
@@ -30,6 +45,13 @@ R<- R_hosps[,1]
 H<- n_days-1
 
 ## OPE, no uncertainty:
+
+if(constrained == TRUE){
+  sink(paste0("new_results/VisRat-OPE_", date, "_", outcome, "_constrained.txt"))
+}else{
+  sink(paste0("new_results/VisRat-OPE_", date, "_", outcome, ".txt"))
+}
+
 
 summary(pg * VR_preds * R / pb)
 hist(log(-pg * VR_preds * R / pb))
@@ -80,14 +102,18 @@ Res_samps_pb<- apply(shuffled_pb, MARGIN=1,
                      function(x) H*mean(pg * VR_preds * R / x))
 quantile(Res_samps_pb, probs=c(0.05, 0.95))
 
-Res_samps_pb_R<- matrix(0, nrow = n, ncol = n_boots)
+n2_boots<- 500
 
-for(j in max(c(1,sum(Res_samps_pb_R[1,]!=0))):n_boots){
+Res_samps_pb_R<- matrix(0, nrow = n, ncol = n2_boots)
+
+for(j in max(c(1,sum(Res_samps_pb_R[1,]!=0))):n2_boots){
   inds<- sample(1:N, N, replace=TRUE)
   Res_samps_pb_R[,j]<- apply(shuffled_pb[,inds], MARGIN=1, 
                        function(x) H*mean(pg[inds] * VR_preds[inds] * R[inds] / x))
   print(j)
 }
 
-quantile(Res_samps_pb_R[,1:sum(Res_samps_pb_R[1,]!=0)], probs=c(0.05, 0.95))
+# quantile(Res_samps_pb_R[,1:sum(Res_samps_pb_R[1,]!=0)], probs=c(0.05, 0.95))
+quantile(Res_samps_pb_R, probs=c(0.05, 0.95))
 
+sink()
