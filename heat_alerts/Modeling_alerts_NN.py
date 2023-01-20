e0033236af3fc144198864b3cd51d560341cc308
Ellen Considine <ellen_considine@g.harvard.edu>
2023-01-19 19:49:16 -0500

    modeling alerts w/ tuning params

--- heat_alerts/Modeling_alerts_NN.py
+++ heat_alerts/Modeling_alerts_NN.py
@@ -137,8 +137,8 @@ def main(params):
     )
 
     config = { # results from tuning
-        "dropout_prob": 0.0,
-        "n_hidden": 256,
+        "dropout_prob": 0.25,
+        "n_hidden": 64,
         "w_decay": 1e-4
     }
 
