needed to fix form of rewards calculation

heat_alerts/Deep_rewards.py
@@ -77,7 +77,7 @@ class DQN_Lightning(pl.LightningModule):
         # preds = self.net(s).gather(1, a.view(-1, 1)).view(-1)
         preds = self.net(s, id)
         random_slopes = F.softplus(self.net.lsigma_slopes)*self.net.randeff_slopes[id]
-        Preds = torch.where(a == 0, preds[:,0], preds[:,0] + F.softplus(preds[:,1] + random_slopes))
+        Preds = torch.where(a == 0, preds[:,0], preds[:,0] - F.softplus(preds[:,1] + random_slopes))
         Preds = Preds + F.softplus(self.net.lsigma)*self.net.randeff[id]
         Preds = -torch.exp(Preds)
         # preds = -F.softplus(self.net(s, id))
@@ -214,7 +214,7 @@ def main(params):
     r_hat = model.net(s,id)
     random_slopes = F.softplus(model.net.lsigma_slopes)*model.net.randeff_slopes[id]
     R_hat = r_hat
-    R_hat[:,1] = R_hat[:,0] + F.softplus(R_hat[:,1] + random_slopes[:,0])
+    R_hat[:,1] = R_hat[:,0] - F.softplus(R_hat[:,1] + random_slopes[:,0])
     random_intercepts = F.softplus(model.net.lsigma)*model.net.randeff[id]
     R_hat = R_hat + random_intercepts
     R_hat = -torch.exp(R_hat)

heat_alerts/Inspect_rewards_model.py
@@ -104,16 +104,16 @@ class DQN_Lightning(pl.LightningModule): # change name!
 
 ## Main code:
 
-deaths_model = torch.load("Fall_results/R_1-23_deaths.pt", map_location=torch.device('cpu'))
-other_hosps_model = torch.load("Fall_results/R_1-23_other-hosps.pt", map_location=torch.device('cpu'))
-all_hosps_model = torch.load("Fall_results/R_1-23_all-hosps.pt", map_location=torch.device('cpu'))
+# deaths_model = torch.load("Fall_results/R_1-23_deaths.pt", map_location=torch.device('cpu'))
+# other_hosps_model = torch.load("Fall_results/R_2-8_other-hosps.pt", map_location=torch.device('cpu'))
+all_hosps_model = torch.load("Fall_results/R_2-8_all-hosps.pt", map_location=torch.device('cpu'))
 
 ## Remove dropout from evaluation of them all:
-deaths_model.eval()
-other_hosps_model.eval()
+# deaths_model.eval()
+# other_hosps_model.eval()
 all_hosps_model.eval()
 
-model = other_hosps_model
+model = all_hosps_model # other_hosps_model
 
 D = make_data(data_only=False)
 S, A, ID = [D[k] for k in ("S", "A", "ID")]
@@ -148,4 +148,4 @@ for h in history_values:
 
 ## Save results:
 df = pd.DataFrame([history_values, Diffs, A1_Diffs])
-df.to_csv("Fall_results/Hosp_preds_vs_History.csv")
+df.to_csv("Fall_results/All-Hosp_preds_vs_History.csv")