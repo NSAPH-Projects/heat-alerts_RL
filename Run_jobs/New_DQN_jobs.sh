8238049dd3841b562583136fcf0ec4345b04b58f
Ellen Considine <ellen_considine@g.harvard.edu>
2023-03-22 16:36:58 -0400

    Last try to get python & d3rlpy to work together

--- Run_jobs/New_DQN_jobs.sh
+++ Run_jobs/New_DQN_jobs.sh
@@ -15,6 +15,6 @@
 source ~/.bashrc
 mymodules
 cd heat-alerts_mortality_RL
-source activate pt1.12_cuda11.6
 singularity shell --bind /n/dominici_nsaph_l3/Lab/projects/heat-alerts_mortality_RL/ --nv d3rlpy_latest.sif --num_gpus=4
+source activate pt1.12_cuda11.6
 python Run_jobs/pylauncher.py --job_file="./Run_jobs/New_DQN_tests" --i $SLURM_ARRAY_TASK_ID
