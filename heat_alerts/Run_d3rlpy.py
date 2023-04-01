95a21d7470d50ae9c0c4e0ba0f20a43723173b51
Ellen Considine <ellen_considine@g.harvard.edu>
2023-04-01 14:25:05 -0400

    removing test params

--- heat_alerts/Run_d3rlpy.py
+++ heat_alerts/Run_d3rlpy.py
@@ -38,15 +38,15 @@ def main(params):
     params = vars(params)
 
     ## For now:
-    params = dict(
-        outcome = "other_hosps", n_hidden = 256,
-        n_gpus=1, b_size=2048, n_epochs=2,
-        lr=0.0001, gamma=1.0, sync_rate = 3,
-        modeled_r = False, random_effects = False,
-        model_name = "test_cpq",
-        eligible = "90pct",
-        algo = "CPQ"
-        )
+#     params = dict(
+#         outcome = "other_hosps", n_hidden = 256,
+#         n_gpus=1, b_size=2048, n_epochs=2,
+#         lr=0.0001, gamma=1.0, sync_rate = 3,
+#         modeled_r = False, random_effects = False,
+#         model_name = "test_cpq",
+#         eligible = "90pct",
+#         algo = "CPQ"
+#         )
 
     ## Prepare data:
     print(params["modeled_r"])
