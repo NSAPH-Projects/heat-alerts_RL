fixing swapped sign

heat_alerts/Deep_rewards_no-RE.py
@@ -69,7 +69,7 @@ class Rewards_Lightning(pl.LightningModule):
     def make_pred_and_targets(self, batch):
         s, a, r = batch
         preds = self.net(s)
-        Preds = torch.where(a == 0, preds[:,0], preds[:,0] + F.softplus(preds[:,1]))
+        Preds = torch.where(a == 0, preds[:,0], preds[:,0] - F.softplus(preds[:,1]))
         Preds = -torch.exp(Preds)
         return Preds, r
     def configure_optimizers(self):
@@ -225,7 +225,7 @@ def main(params):
     model.eval() # turns off dropout for the predictions
     r_hat = model.net(s)
     R_hat = r_hat
-    R_hat[:,1] = R_hat[:,0] + F.softplus(R_hat[:,1])
+    R_hat[:,1] = R_hat[:,0] - F.softplus(R_hat[:,1])
     R_hat = -torch.exp(R_hat)
     n = R_hat.detach().numpy()
     df = pd.DataFrame(n)