63315975781ebdd38f4694c40a19e9fd7431e04f
Ellen Considine <ellen_considine@g.harvard.edu>
2023-06-28 12:42:56 -0600

    Using gpu

--- Run_jobs/R_jobs.sh
+++ Run_jobs/R_jobs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -J R_model
 #SBATCH -N 1
-#SBATCH --ntasks-per-node 10
-#SBATCH -p test # fasse_gpu
-#SBATCH -t 0-7:00 # 0-8:00
-#SBATCH --mem 92G # 50G
-#SBATCH --gres gpu:0 # gpu:1
+#SBATCH --ntasks-per-node 4
+#SBATCH -p fasse_gpu
+#SBATCH -t 0-10:00 # 0-8:00
+#SBATCH --mem 370G # 50G
+#SBATCH --gres gpu:1 # gpu:0
 #SBATCH --array 0 # 0-2
 #SBATCH -o ./Run_jobs/slurm/r.%a.out
 #SBATCH --mail-user=ellen_considine@g.harvard.edu
