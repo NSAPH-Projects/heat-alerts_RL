505f702eb397ed652c8ac06fd42b857858adb0e1
Ellen Considine <ellen_considine@g.harvard.edu>
2023-07-04 11:10:45 -0600

    Not encouraging R1 >= R0

--- heat_alerts/Deep_rewards_no-RE.py
+++ heat_alerts/Deep_rewards_no-RE.py
@@ -70,7 +70,8 @@ class Rewards_Lightning(pl.LightningModule):
         s, a, r = batch
         preds = self.net(s)
         # Preds = torch.where(a == 0, preds[:,0], preds[:,0] - F.softplus(preds[:,1]))
-        Preds = torch.where(a == 0, preds[:,0], preds[:,0] + preds[:,1])
+        # Preds = torch.where(a == 0, preds[:,0], preds[:,0] + preds[:,1])
+        Preds = torch.where(a == 0, preds[:,0], preds[:,1])
         Preds = -torch.exp(Preds)
         return Preds, r
     def configure_optimizers(self):
@@ -229,7 +230,7 @@ def main(params):
     r_hat = model.net(s)
     R_hat = r_hat
     # R_hat[:,1] = R_hat[:,0] - F.softplus(R_hat[:,1])
-    R_hat[:,1] = R_hat[:,0] + R_hat[:,1]
+    # R_hat[:,1] = R_hat[:,0] + R_hat[:,1]
     R_hat = -torch.exp(R_hat)
     n = R_hat.detach().numpy()
     df = pd.DataFrame(n)
