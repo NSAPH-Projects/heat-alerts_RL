174bf64acfa2e06be094bcd637685206305bc88c
mauriciogtec <mauriciogtec@hsph.harvard.edu>
2023-07-13 17:47:05 -0400

    improved nn model which contains now the simple and base case as special cases

--- bayesian_model/fit_data_pyro_nn.py
+++ bayesian_model/fit_data_pyro_nn.py
@@ -1,6 +1,6 @@
-
 import argparse
 import json
+import os
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -25,93 +25,69 @@ def train(model, guide, data, lr, n_epochs, batch_size, num_particles=1):
     loss_fn(*data)  # initialize parameters
     num_pars = [p.numel() for p in loss_fn.parameters() if p.requires_grad]
     print("Number of parameters: ", sum(num_pars))
-    opt = torch.optim.Adam(loss_fn.parameters(), lr=lr)
-
-    # # Create a dataloader.
     dataset = torch.utils.data.TensorDataset(*data)
-    dataloader = torch.utils.data.DataLoader(
-        dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        pin_memory=True,
-        num_workers=8,
-        prefetch_factor=8,
-        persistent_workers=True,
-    )
-
-    epoch_loss = np.nan
-    for epoch in range(n_epochs):
+    # # Create a dataloader.
+    if batch_size is not None:
+        opt = torch.optim.Adam(loss_fn.parameters(), lr=lr)
+        dataloader = torch.utils.data.DataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            pin_memory=True,
+            num_workers=8,
+            prefetch_factor=8,
+            persistent_workers=True,
+        )
         epoch_losses = []
-        # add epoch indicator in tqdm
-        pbar = tqdm(dataloader, leave=False)
-        for batch in pbar:
-            opt.zero_grad()
-            reg = 1e-4 * sum([x.pow(2).sum() for x in loss_fn.guide.parameters()])
-            elbo_loss = loss_fn(*batch)
-            loss = elbo_loss + reg
-            loss.backward()
-            torch.nn.utils.clip_grad_value_(loss_fn.parameters(), 1.0)
-            opt.step()
-            epoch_losses.append(loss.item())
-            pbar_desc = f"[epoch {epoch + 1}/{n_epochs}, {loss:.4f}]"
-            pbar.set_description(pbar_desc, refresh=False)
-        epoch_loss = sum(epoch_losses) / len(epoch_losses)
-        if epoch == 0 or ((epoch + 1) % (n_epochs // 10)) == 0:
-            print(
-                "[epoch {}/{}]  av. loss: {:.4f}".format(
-                    epoch + 1, n_epochs, epoch_loss
+        epoch_loss = np.nan
+        for epoch in range(n_epochs):
+            losses_buffer = []
+            # add epoch indicator in tqdm
+            pbar = tqdm(dataloader, leave=False)
+            for batch in pbar:
+                opt.zero_grad()
+                reg = 1e-4 * sum([x.pow(2).sum() for x in loss_fn.guide.parameters()])
+                elbo_loss = loss_fn(*batch)
+                loss = elbo_loss + reg
+                loss.backward()
+                torch.nn.utils.clip_grad_value_(loss_fn.parameters(), 1.0)
+                opt.step()
+                losses_buffer.append(loss.item())
+                pbar_desc = f"[epoch {epoch + 1}/{n_epochs}, {loss:.4f}]"
+                pbar.set_description(pbar_desc, refresh=False)
+            epoch_loss = sum(losses_buffer) / len(losses_buffer)
+            epoch_losses.append(epoch_loss)
+            if epoch == 0 or ((epoch + 1) % min(n_epochs // 10, 1) == 0):
+                print(
+                    "[epoch {}/{}]  av. loss: {:.4f}".format(
+                        epoch + 1, n_epochs, epoch_loss
+                    )
                 )
-            )
-
-
-# class LowRankGuideNudge(pyro.infer.autoguide.AutoContinuous):
-#     # scale_constraint = constraints.positive
-
-#     def __init__(self, model, init_loc_fn=init_to_median, init_scale=0.1, rank=None):
-#         if not isinstance(init_scale, float) or not (init_scale > 0):
-#             raise ValueError("Expected init_scale > 0. but got {}".format(init_scale))
-#         if not (rank is None or isinstance(rank, int) and rank > 0):
-#             raise ValueError("Expected rank > 0 but got {}".format(rank))
-#         self._init_scale = init_scale
-#         self.rank = rank
-#         super().__init__(model, init_loc_fn=init_loc_fn)
-
-#     def _setup_prototype(self, *args, **kwargs):
-#         super()._setup_prototype(*args, **kwargs)
-#         # Initialize guide params
-#         self.loc = nn.Parameter(self._init_loc())
-#         if self.rank is None:
-#             self.rank = int(round(self.latent_dim**0.5))
-#         self.prescale = PyroParam(
-#             self.loc.new_full((self.latent_dim,), 0.5**0.5 * self._init_scale),
-#             # constraint=self.scale_constraint,
-#         )
-#         self.cov_factor = nn.Parameter(
-#             self.loc.new_empty(self.latent_dim, self.rank).normal_(
-#                 0, 1 / self.rank**0.5
-#             )
-#         )
-
-#     def transformed_scale(self):
-#         return 5.0 * torch.sigmoid(self.prescale)
-
-#     def get_posterior(self, *args, **kwargs):
-#         """
-#         Returns a LowRankMultivariateNormal posterior distribution.
-#         """
-#         scale = self.transformed_scale()
-#         cov_factor = self.cov_factor * scale.unsqueeze(-1)
-#         cov_diag = scale * scale + 1e-3
-#         return dist.LowRankMultivariateNormal(self.loc, cov_factor, cov_diag)
-
-#     def _loc_scale(self, *args, **kwargs):
-#         scale = self.transformed_scale()
-#         scale = scale * (self.cov_factor.pow(2).sum(-1) + 1).sqrt()
-#         return self.loc, scale
+        return epoch_losses
+    else:
+        epoch_loss = np.nan
+        pbar = tqdm(range(n_epochs), leave=False)
+        opt = pyro.optim.Adam({"lr": lr})
+        svi = pyro.infer.SVI(
+            model, guide, opt, loss=pyro.infer.Trace_ELBO(num_particles=num_particles)
+        )
+        losses_buffer = []
+        for epoch in pbar:
+            epoch_loss = svi.step(*data)  # for some reason currently complaining
+            pbar_desc = f"[epoch {epoch + 1}/{n_epochs}, {epoch_loss:.4f}]"
+            pbar.set_description(pbar_desc, refresh=False)
+            if epoch == 0 or ((epoch + 1) % (n_epochs // 10)) == 0:
+                print(
+                    "[epoch {}/{}]  av. loss: {:.4f}".format(
+                        epoch + 1, n_epochs, epoch_loss
+                    )
+                )
+            losses_buffer.append(epoch_loss)
+        return losses_buffer
 
 
 class Model(pyro.nn.PyroModule):
-    def __init__(self, W, X):
+    def __init__(self, W, X, num_hidden_layers, hdim, sample_weights=False):
         super().__init__()
         N, DX = X.shape
         S, DW = W.shape
@@ -120,25 +96,36 @@ class Model(pyro.nn.PyroModule):
         self.DX = DX
         self.DW = DW
         self.W = W
-        self.nn_beta = pyro.nn.PyroModule[nn.Sequential](
-            pyro.nn.PyroModule[nn.Linear](DW, 32),
-            nn.ReLU(),
-            pyro.nn.PyroModule[nn.Linear](32, DX),
-        )
-        self.nn_gamma = pyro.nn.PyroModule[nn.Sequential](
-            pyro.nn.PyroModule[nn.Linear](DW, 32),
-            nn.ReLU(),
-            pyro.nn.PyroModule[nn.Linear](32, DX),
-        )
 
+        def make_nn(indim, outdim, hdim, num_hidden, sample_weights=False, act=nn.ReLU):
+            module = []
+            for i in range(num_hidden):
+                d = indim if i == 0 else hdim
+                module.append(nn.Linear(d, hdim))
+                module.append(act())
+            d = indim if num_hidden == 0 else hdim
+            module.append(nn.Linear(d, outdim))
+            module = nn.Sequential(*module)
+            pyro.nn.module.to_pyro_module_(module, recurse=True)
+
+            if sample_weights:
+                for name, value in list(module.named_parameters(recurse=False)):
+                    setattr(
+                        module,
+                        name,
+                        pyro.nn.PyroSample(
+                            prior=dist.Normal(0, 1)
+                            .expand(value.shape)
+                            .to_event(value.dim())
+                        ),
+                    )
+
+            return module
+
+        self.nn_beta = make_nn(DW, DX, hdim, num_hidden_layers, sample_weights)
+        self.nn_gamma = make_nn(DW, DX, hdim, num_hidden_layers, sample_weights)
 
     def forward(self, A, X, offset, sind, Y=None, return_all=False):
-        # omega_beta = pyro.sample(
-        #     "omega_beta", dist.InverseGamma(1.1, 1.1).expand([self.DX]).to_event(1)
-        # )
-        # omega_gamma = pyro.sample(
-        #     "omega_gamma", dist.InverseGamma(1.1, 1.1).expand([self.DX]).to_event(1)
-        # )
         omega_beta = pyro.sample(
             "omega_beta", dist.HalfCauchy(1.0).expand([self.DX]).to_event(1)
         )
@@ -169,7 +156,7 @@ class Model(pyro.nn.PyroModule):
 
 def main(args):
     # %% read processed data
-    dir = "bayesian_model/data/processed/"
+    dir = "data/processed/"
     X = pd.read_parquet(f"{dir}/states.parquet")
     A = pd.read_parquet(f"{dir}/actions.parquet")
     Y = pd.read_parquet(f"{dir}/outcomes.parquet")
@@ -188,13 +175,13 @@ def main(args):
     # render pyro model
     S, DW = W_.shape
     N, DX = X_.shape
-    model = Model(W_, X_)
+    model = Model(W_, X_, args.num_hidden_layers, args.hidden_dim, args.sample_weights)
     inputs = [A_, X_, offset_, sind_, Y_]
     # pyro.render_model(
     #     model,
     #     model_args=inputs,
     #     render_distributions=True,
-    #     filename="bayesian_model/fit_data_pyro_diag.png",
+    #     filename="fit_data_pyro_diag.png",
     # )
 
     # inference with autoguide
@@ -214,7 +201,7 @@ def main(args):
         inputs,
         lr=args.lr,
         n_epochs=args.n_epochs,
-        batch_size=X.shape[0] // S,
+        batch_size=X.shape[0] // S if not args.full_batch else None,
         num_particles=args.num_particles,
     )
 
@@ -228,17 +215,25 @@ def main(args):
     params = Predictive(model, guide=guide, num_samples=50, return_sites=sites)(*inputs)
     params = {k: v[:, 0] for k, v in params.items()}
 
+    betas = params["beta"]
+    gammas = params["gamma"]
+
+    betas_means = betas.mean((0, 1))
+    betas_std = betas.std((0, 1))
+    gammas_means = gammas.mean((0, 1))
+    gammas_std = gammas.std((0, 1))
+
     # save a json of all samples on sites, make sure to save as float
     results = {}
     for s in sites:
         results[s] = params[s].numpy().astype(float).tolist()
-    with open("bayesian_model/fit_data_pyro_nn.json", "w") as io:
+    with open(f"Plots_params/fit_data_pyro_{args.name}.json", "w") as io:
         json.dump(results, io)
 
     predictive_outputs = Predictive(
         model,
         guide=guide,
-        num_samples=50,
+        num_samples=args.n_samples,
         return_sites=["_RETURN"],
     )
     outputs = predictive_outputs(*inputs, return_all=True)["_RETURN"]
@@ -256,11 +251,7 @@ def main(args):
     ax[3].set_xlabel("mu")
     ax[3].set_ylabel("real obs")
 
-    fig.savefig("bayesian_model/fit_data_pyro_nn.png", bbox_inches="tight")
-
-    # make errorplot of gamma and beta coefficients
-    betas = params["beta"]
-    gammas = params["gamma"]
+    fig.savefig(f"Plots_params/fit_data_pyro_{args.name}.png", bbox_inches="tight")
 
     betas_means = betas.mean((0, 1))
     betas_std = betas.std((0, 1))
@@ -278,7 +269,7 @@ def main(args):
     )
     ax[0].set_xticks(np.arange(len(colnames)))
     ax[0].set_xticklabels(colnames, rotation=45)
-    ax[0].set_title("heat alert effectiveness")
+    ax[0].set_title("baseline rate")
     ax[1].errorbar(
         np.arange(len(colnames)),
         gammas_means[colidx],
@@ -286,31 +277,57 @@ def main(args):
         fmt="o",
     )
     ax[1].set_xticks(np.arange(len(colnames)))
-    ax[1].set_title("expected baseline hospitalizations")
-    fig.savefig("bayesian_model/fit_data_pyro_coefficients_nn.png", bbox_inches="tight")
+    ax[1].set_xticklabels(colnames, rotation=45)
+    ax[1].set_title("heat alert effectiveness")
+    fig.savefig(
+        f"Plots_params/fit_data_pyro_coefficients_{args.name}.png", bbox_inches="tight"
+    )
 
     # load spline design matrix
-    dos = pd.read_parquet("bayesian_model/data/processed/Btdos.parquet")  # T x num feats
-    dos = torch.tensor(dos.values, dtype=torch.float32)
-    dos_cols = np.array([i for i, c in enumerate(X.columns) if c.startswith("dos")], dtype=int)
-    dos_beta = betas[..., dos_cols].reshape(-1, 1, len(dos_cols))  #  (num samples*locs) x n1 x um feats
-    dos_eff = (dos_beta * dos).sum(-1)  # num samples x T
+    dos = pd.read_parquet("data/processed/Btdos.parquet").values  # T x num feats
+
+    dos_cols = np.array(
+        [i for i, c in enumerate(X.columns) if c.startswith("dos")], dtype=int
+    )
+    dos_beta = betas[..., dos_cols].reshape(
+        -1, 1, len(dos_cols)
+    )  #  (num samples*locs) x n1 x um feats
+    dos_gamma = gammas[..., dos_cols].reshape(
+        -1, 1, len(dos_cols)
+    )  #  (num samples*locs) x n1 x um feats
+    dos_beta_eff = (dos_beta * dos).sum(-1)  # num samples x T
+    dos_gamma_eff = (dos_gamma * dos).sum(-1)  # num samples x T
 
     # plot as spaghetti, gray, alpha
-    ix = np.random.choice(dos_eff.shape[0], 200, replace=False)
-    fig, ax = plt.subplots(figsize=(8, 4))
-    ax.plot(dos_eff[ix].T, color="k", alpha=0.1, lw=0.5)
-    ax.plot(dos_eff.mean(0), color="k", lw=2)
-    ax.set_xlabel("Day of summer")
-    ax.set_title("Day of summer effect")
-    fig.savefig("bayesian_model/fit_data_pyro_splines_nn.png", bbox_inches="tight")
+    ix = np.random.choice(dos_beta_eff.shape[0], 200, replace=False)
+    fig, ax = plt.subplots(1, 2, figsize=(12, 4))
+    ax[0].plot(dos_beta_eff[ix].T, color="k", alpha=0.1, lw=0.5)
+    ax[0].plot(dos_beta_eff.mean(0), color="k", lw=2)
+    ax[0].set_xlabel("Day of summer")
+    ax[0].set_title("Baseline rate")
+    ax[1].plot(dos_gamma_eff[ix].T, color="k", alpha=0.1, lw=0.5)
+    ax[1].plot(dos_gamma_eff.mean(0), color="k", lw=2)
+    ax[1].set_xlabel("Day of summer")
+    ax[1].set_title("Heat alert effectiveness")
+    fig.savefig(
+        f"Plots_params/fit_data_pyro_splines_{args.name}.png", bbox_inches="tight"
+    )
 
+    os.makedirs("../Bayesian_models", exist_ok=True)
+    torch.save(model, f"../Bayesian_models/Pyro_model_{args.name}.pt")
+    torch.save(guide, f"../Bayesian_models/Pyro_guide_{args.name}.pt")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--n_epochs", type=int, default=40)
+    parser.add_argument("--n_epochs", type=int, default=30)
+    parser.add_argument("--n_samples", type=int, default=50)
     parser.add_argument("--lr", type=float, default=0.001)
     parser.add_argument("--num_particles", type=int, default=10)
+    parser.add_argument("--num_hidden_layers", type=int, default=0)
+    parser.add_argument("--hidden_dim", type=int, default=0)
+    parser.add_argument("--name", type=str, default="nn")
+    parser.add_argument("--full_batch", default=False, action="store_true")
+    parser.add_argument("--sample_weights", default=False, action="store_true")
     args = parser.parse_args()
     main(args)
