b6710c589f218f4608748120ec7d41d35fd7f613
Ellen Considine <ellen_considine@g.harvard.edu>
2023-08-10 09:26:24 -0600

    Less jobs

--- Run_jobs/Online_RL.sh
+++ Run_jobs/Online_RL.sh
@@ -5,7 +5,7 @@
 #SBATCH -p fasse # test
 #SBATCH -t 0-8:00
 #SBATCH --mem 60G
-#SBATCH --array 0-59 
+#SBATCH --array 0-47 
 #SBATCH -o ./Run_jobs/slurm/orl.%a.out
 #SBATCH --mail-user=ellen_considine@g.harvard.edu
 #SBATCH --mail-type=BEGIN
