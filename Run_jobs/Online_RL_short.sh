4fd0a9dbe0ebbaecd1dc4e79179b9cc216664378
Ellen Considine <ellen_considine@g.harvard.edu>
2023-08-29 18:51:26 -0400

    Update Online_RL_short.sh

--- Run_jobs/Online_RL_short.sh
+++ Run_jobs/Online_RL_short.sh
@@ -3,9 +3,9 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -p fasse # test
-#SBATCH -t 0-0:10 # 0-5:00
+#SBATCH -t 0-5:00
 #SBATCH --mem 6G
-#SBATCH --array 0-219 # 54 # 23
+#SBATCH --array 0-43 # 54 # 23
 #SBATCH -o ./Run_jobs/slurm/orl_short.%a.out
 #SBATCH --mail-user=ellen_considine@g.harvard.edu
 #SBATCH --mail-type=BEGIN
