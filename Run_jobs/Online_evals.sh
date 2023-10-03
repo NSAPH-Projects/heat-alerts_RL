93ec73c561cfa0aab63ce9f6ebd8ec8b69965500
Ellen Considine <ellen_considine@g.harvard.edu>
2023-10-03 12:45:43 -0400

    Create Online_evals.sh

---
+++ Run_jobs/Online_evals.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J Orl_short
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -p fasse 
+#SBATCH -t 0-0:30 
+#SBATCH --mem 3G 
+#SBATCH --array 0-1319 
+#SBATCH -o ./Run_jobs/slurm/orl_short.%a.out
+#SBATCH --mail-user=ellen_considine@g.harvard.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#
+source ~/.bashrc
+mymodules
+cd heat-alerts_mortality_RL
+source activate heatrl
+python Run_jobs/pylauncher.py --job_file="./Run_jobs/Online_tests_short" --i $SLURM_ARRAY_TASK_ID
