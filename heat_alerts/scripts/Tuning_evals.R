6aef0ef94cd727a8bd116f2b329161dfe28fbbbb
Ellen Considine <econsidine@boslogin02.rc.fas.harvard.edu>
2023-10-16 08:40:49 -0400

    filling in tuning exps

--- heat_alerts/scripts/Tuning_evals.R
+++ heat_alerts/scripts/Tuning_evals.R
@@ -1,6 +1,8 @@
 
 source("heat_alerts/scripts/Evaluation_functions.R")
 
+type<- "evaluation"
+
 ## Change manually:
 eval_func_name<- "compare_to_zero" 
 # eval_func_name<- "avg_return"
@@ -55,7 +57,7 @@ for(i in 1:nrow(DF)){
   }
 }
 
-write.csv(DF, paste0("Fall_results/Tuning_evals_", eval_func_name, ".csv"))
+write.csv(DF, paste0("Fall_results/Tuning_evals_", type, "_", eval_func_name, ".csv"))
 
 
 ## If eval_func == compare_to_zero:
@@ -79,11 +81,12 @@ for(i in 1:nrow(DF)){
   }
 }
 
-write.csv(DF, paste0("Fall_results/Tuning_evals_", eval_func_name, ".csv"))
+write.csv(DF, paste0("Fall_results/Tuning_evals_", type, "_", eval_func_name, ".csv"))
 
 
-#### Inspect results: regress Eval on c(tune_forecasts, NHU, NHL, LR, gamma, n_steps),
-  # after selecting the best HI threshold for each?
+#### Select the best HI threshold for each county-hyperparameter combo:
+
+library(dplyr)
 
 tune_counties<- c(32003, 29019, 45015, 19153, 41053)
 
@@ -91,7 +94,9 @@ tune_counties<- c(32003, 29019, 45015, 19153, 41053)
 eval_func_name<- "compare_to_zero" 
 # eval_func_name<- "avg_return"
 
-DF<- read.csv(paste0("Fall_results/Tuning_evals_", eval_func_name, ".csv"))[,-1]
+DF<- read.csv(paste0("Fall_results/Tuning_evals_", type, "_", eval_func_name, ".csv"))[,-1]
+
+DF$Index<- 1:nrow(DF)
 
 vars<- c("tune_counties", "tune_forecasts", "NHU", "NHL", "LR", "gamma", "n_steps")
 opt_DF<- distinct(DF[,vars])
@@ -100,6 +105,64 @@ opt_DF$Eval<- 0
 opt_DF$OT<- 0
 
 for(j in 1:nrow(opt_DF)){
-  pos<- which(DF[,vars] == opt_DF[j, vars])
+  pos<- inner_join(opt_DF[j,vars], DF[,c(vars, "Index")], by=vars)$Index
+  m<- which.max(DF[pos, "Eval"])
+  opt_DF[j, "Eval"]<- DF[pos[m],"Eval"]
+  opt_DF[j, "OT"]<- DF[pos[m],"tune_HI"]
+  
+  if(j %% 50 == 0){
+    print(j)
+  }
+}
+
+write.csv(opt_DF, paste0("Fall_results/Tuning_", type, "_opt-HI_", eval_func_name, ".csv"), row.names=FALSE)
+
+#### Inspect results: regress Eval on c(tune_forecasts, NHU, NHL, LR, gamma, n_steps)
+
+library(ggplot2)
+
+# fit0<- lm(Eval ~ tune_counties + tune_forecasts + NHU + NHL +
+#             LR + gamma + n_steps, opt_DF)
+
+for(k in tune_counties){
+  print(k)
+  
+  pos<- which(opt_DF[,"tune_counties"] == k)
+  m<- which.max(opt_DF[pos, "Eval"])
+  print(opt_DF[pos[m],])
+  print(summary(opt_DF[pos,"Eval"]))
+  
+  # fit<- lm(Eval ~ tune_forecasts + NHU + NHL + NHU*NHL +
+  #            LR + gamma + n_steps, opt_DF[which(opt_DF$tune_counties == k),])
+  # print(summary(fit))
+}
+
+
+library(nlme)
+
+lmm.0<- lme(Eval ~ tune_forecasts + NHU + NHL + NHU*NHL + as.factor(n_steps),
+          random = ~1|tune_counties, opt_DF)
+
+lmm.1<- lme(Eval ~ tune_forecasts + NHU + NHL + NHU*NHL + as.factor(n_steps),
+            random = ~1 + as.factor(n_steps)|tune_counties, opt_DF)
+
+lmm.2<- lme(Eval ~ tune_forecasts + NHU + as.factor(NHL) + as.factor(n_steps),
+            random = ~1 + as.factor(n_steps)|tune_counties, opt_DF) # best for training set
+
+# lmm.3<- lme(Eval ~ tune_forecasts + NHU + NHL + NHU*NHL + as.factor(n_steps),
+#             random = ~1 + tune_forecasts|tune_counties, opt_DF)
+# 
+# lmm.4<- lme(Eval ~ tune_forecasts + NHU + NHL + NHU*NHL + as.factor(n_steps),
+#             random = ~1 + NHL|tune_counties, opt_DF)
+
+preds<- rowSums(lmm.2$fitted)
+
+for(k in tune_counties){
+  print(k)
+  
+  pos<- which(opt_DF[,"tune_counties"] == k)
+  m<- which.max(preds[pos])
+  print(opt_DF[pos[m],])
+  print(summary(opt_DF[pos,"Eval"]))
 }
 
