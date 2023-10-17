f6a497cde9899c37f5efee5d83a27505da0c2a0e
Ellen Considine <econsidine@holylogin03.rc.fas.harvard.edu>
2023-10-17 18:56:58 -0400

    prepping for rl Rune

--- heat_alerts/online_rl/Online_print_RLs.R
+++ heat_alerts/online_rl/Online_print_RLs.R
@@ -9,17 +9,18 @@ r_model<- "mixed_constraints"
 
 HI_thresholds<- seq(0.5, 0.9, 0.05)
 Forecasts<- c("Q_D10") # "none"
-# NHU<- c(16, 32, 64)
-# NHL<- c(1, 2, 3)
-# n_steps<- c(1024, 2048, 4096)
-NHU<- c(32)
-NHL<- c(2)
-n_steps<- c(2048)
+NHU<- c(16, 32, 64)
+NHL<- c(1, 2, 3)
+n_steps<- c(1024, 2048, 4096)
+# NHU<- c(32)
+# NHL<- c(2)
+# n_steps<- c(2048)
 
 missing<- c()
 i<- 1
 
-sink("Run_jobs/Online_tests_short") 
+# sink("Run_jobs/Online_tests_short") 
+sink("Run_jobs/Online_tuning") 
 for(k in counties){
   county<- k
   
@@ -53,15 +54,18 @@ for(k in counties){
                          "_arch-", nhl, "-", nhu, "_ns-", s,
                          "_fips-", county)
               if(!file.exists(d)){
-                missing<- append(missing, i)
-                cat(paste0("python train_online_rl_sb3.py", " county=", county,
-                           " restrict_days=qhi", " forecasts=", forecasts, " restrict_days.HI_restriction=", h, 
-                           " algo.policy_kwargs.net_arch=", arch, " algo.n_steps=", s,
-                           " model_name=Tune_F-", forecasts, "_Rstr-HI-", h, 
-                           "_arch-", nhl, "-", nhu, "_ns-", s,
-                           "_fips-", county, " \n"))
-              }
-              i<- i+1
+                if(nhl==2 & nhu==32 & s==2048){
+                }else{
+                  missing<- append(missing, i)
+                  cat(paste0("python train_online_rl_sb3.py", " county=", county,
+                             " restrict_days=qhi", " forecasts=", forecasts, " restrict_days.HI_restriction=", h, 
+                             " algo.policy_kwargs.net_arch=", arch, " algo.n_steps=", s,
+                             " model_name=Tune_F-", forecasts, "_Rstr-HI-", h, 
+                             "_arch-", nhl, "-", nhu, "_ns-", s,
+                             "_fips-", county, " \n"))
+                }
+                i<- i+1
+                }
             }
           }
         }
@@ -75,7 +79,7 @@ sink()
 
 ##########################################
 
-sink("Run_jobs/Online_tests_short") 
+sink("Run_jobs/Online_tests_short")
 for(k in counties){
   county<- k
   
