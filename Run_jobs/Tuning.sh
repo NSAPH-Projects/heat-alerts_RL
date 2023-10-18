f3e2e470b85aeccd2f8490698bdb4c435c9ecd41
Ellen Considine <ellen_considine@g.harvard.edu>
2023-10-18 17:18:34 -0400

    Update Tuning.sh

--- Run_jobs/Tuning.sh
+++ Run_jobs/Tuning.sh
@@ -3,9 +3,9 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -p shared # fasse # test
-#SBATCH -t 0-10:00 # 0-7:00 
+#SBATCH -t 0-15:00 # 0-7:00 
 #SBATCH --mem 6G
-#SBATCH --array 0-6752 # 7289
+#SBATCH --array 0-5969 # 7289
 #SBATCH -o ./Run_jobs/slurm/orl_tune.%a.out
 #SBATCH --mail-user=ellen_considine@g.harvard.edu
 #SBATCH --mail-type=BEGIN
