eb3a1a3565feda23d33032f3f502179d4a12da5c
Ellen Considine <ellen_considine@g.harvard.edu>
2023-11-14 14:07:20 -0500

    Update Prep_DoE_zones.R

--- heat_alerts/Data_Processing/Prep_DoE_zones.R
+++ heat_alerts/Data_Processing/Prep_DoE_zones.R
@@ -1,8 +1,6 @@
 
 library(stringr)
 
-setwd("C:/Users/ellen/OneDrive/MyDocs/Graduate Research/Heat alerts RL")
-
 zones<- read.csv("DoE_climate_zones.csv") # downloaded from https://gist.github.com/philngo/d3e251040569dba67942 on 7/20/22
 
 states<- str_pad(zones$State.FIPS, 2, pad = "0")
