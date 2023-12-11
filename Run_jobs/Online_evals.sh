4821618341ea97f0fa7ef543f4d41b6ce80d723c
Ellen Considine <ellen_considine@g.harvard.edu>
2023-12-11 15:49:35 -0700

    Update Online_evals.sh

--- Run_jobs/Online_evals.sh
+++ Run_jobs/Online_evals.sh
@@ -5,7 +5,7 @@
 #SBATCH -p shared # fasse 
 #SBATCH -t 0-0:30 
 #SBATCH --mem 3G 
-#SBATCH --array 0-539 # 4967 # 9999
+#SBATCH --array 0-329 # 4967 # 9999
 #SBATCH -o ./Run_jobs/slurm/evals.%a.out
 #SBATCH --mail-user=ellen_considine@g.harvard.edu
 #SBATCH --mail-type=BEGIN
