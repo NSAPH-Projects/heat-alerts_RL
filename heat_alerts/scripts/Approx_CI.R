19c36a03f2740cd8672b04c4e8bd3941119b2d1c
Ellen Considine <ellen_considine@g.harvard.edu>
2024-01-17 12:18:31 -0700

    Create Approx_CI.R

---
+++ heat_alerts/scripts/Approx_CI.R
@@ -0,0 +1,48 @@
+
+agg_results<- read.csv("Fall_results/December_Rstr-QHI_RL_avg_return.csv")
+
+Agg_results<- agg_results[which(agg_results$Algo=="trpo" & agg_results$Forecast=="none"),]
+
+## Get and scramble episode rewards:
+rl_df<- matrix(0, nrow=1000, ncol=30)
+nws_df<- matrix(0, nrow=1000, ncol=30)
+
+for(i in 1:30){
+  county<- Agg_results$County[i]
+  dat<- read.csv(paste0("Summer_results/ORL_RL_eval_samp-R_obs-W_December_",
+                        "trpo", "_F-", "none", "_Rstr-HI-", Agg_results$OT[i],
+                        "_arch-", agg_results$NHL[i], "-", agg_results$NHU[i], "_ns-", agg_results$n_steps[i],
+                        "_fips-", county, "_fips_", county, ".csv"))
+  nws<- read.csv(paste0("Summer_results/ORL_NWS_eval_samp-R_obs-W_", "mixed_constraints", "_fips_", county, ".csv"))
+  
+  dat$Index<- rep(1:1000, each=152)
+  nws$Index<- rep(1:1000, each=152)
+  
+  rl_df[,i]<- tapply(dat$Rewards, dat$Index, sum)
+  nws_df[,i]<- tapply(nws$Rewards, dat$Index, sum)
+
+  print(i)
+}
+
+## Convert to absolute hosps:
+W<- read.csv("data/Final_30_W.csv")
+
+W<- W[match(Agg_results$County, W$Fips),]
+
+get_hosps<- function(x){ # where x is a vector from avg_return function
+  y<- (1 - x/152)*W$Offset/W$total_count
+  return(y*152) # per summer
+}
+
+rl_DF<- apply(rl_df, MARGIN=1, get_hosps)*10000
+nws_DF<- apply(nws_df, MARGIN=1, get_hosps)*10000
+
+DF<- nws_DF - rl_DF
+# DF<- apply(DF, MARGIN=1, function(x){sample(x)})
+
+## Calculate medians --> approximate CI
+
+m<- apply(DF, MARGIN=1, median)
+quantile(m, probs = c(0.025, 0.975))
+quantile(m, probs = c(0.025, 0.975))*4900
+
